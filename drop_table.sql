08106c829cdc7ac56c6f12f4027f7e5d9d664fc4
GrandLaite <53825459+GrandLaite@users.noreply.github.com>
2025-03-17 08:52:13 +0600

    Create drop_table.sql

---
+++ drop_table.sql
@@ -0,0 +1,8 @@
+-- Удаляем базу данных, для которой мы создавали роли и пишем
+DROP ROLE IF EXISTS 
+    supplier_role, 
+    seller_role, 
+    admin_role, 
+    merchandiser_role, 
+    admin_it_role, 
+    sup1;
